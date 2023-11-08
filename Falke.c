9c37e0a886a893df90a26f2165a1e26defff73f1
MYCROMARS-OPERATIONS <alex.r@mycromars-operations.com>
2023-11-08 17:24:58 +0100

    Exit or continue

--- Falke.c
+++ Falke.c
@@ -1,14 +1,21 @@
 // Falke 2023 v.1.0.0
 #include <stdio.h>
+#include <stdlib.h>
 
 int a = 0;
 int b = 0;
 int sum = 0;
 
+char buchstabe;
+
+int exitProg = 0;
+
 int main() {
     printf("\n");
     printf("    MMO Falke 2023\n");
-     printf("**********************\n\n");
+    printf("**********************\n\n");
+
+    // Calculation a+b
     printf("Calculation 1: a+b\n");
 
     printf("Input a: ");
@@ -21,5 +28,17 @@ int main() {
     printf("Summe: ");
     printf("%d\n\n", sum);
 
-    return (0);
+    // Exit or continue
+    printf("Press 1 for exit, 2 for continue: ");
+    scanf("%d", &exitProg);
+
+    if (exitProg == 1){
+        printf("End\n");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        printf("Continue\n");
+    }
+
+    return 0;
 }
