0a2e3d0c5dee1cb77bf1372315c565f7c50b3af3
MYCROMARS-OPERATIONS <alex.r@mycromars-operations.com>
2023-11-08 21:49:38 +0100

    Menu 2

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+// Step every 1000
+            // if (i > st * step)
+            // {
+            //     st++;
+            // }
+            // // Print every Step
+            // if (i = st * step)
+            // {
+            //     printf("Timer: %d\n", i);
+            // }
