d7dc23a81c85bc0e4cf3a0ba92cd1ddc3c6711da
Alexander Rudi <149962078+MYCROMARS-OPERATIONS@users.noreply.github.com>
2024-08-10 17:28:15 +0200

    time

--- source/time_a.c
+++ source/time_a.c
@@ -6,20 +6,24 @@
 // Calculation
 void time_a(void)
 {
-	// typedef
+	// typedef long int
+	// number of sec since Jan. 1, 1970 midnight
     time_t now = time(NULL);
 
-	// number of sec since Jan. 1, 1970 midnight
 	printf("%s\n", "Seconds since Jan. 1, 1970 midnight:");
 	printf("%ld\n\n", now);
 
 	printf("%s\n", "Programm sleeps 2 seconds");
-    // Programm sleeps 2 seconds
+
+    // Programm sleeps 2 seconds, #include <synchapi.h>
 	Sleep(2000);
+
 	time_t two_secs = time(NULL);
 	printf("%ld\n\n", two_secs);
 
 	printf("%s\n", "Show differenz:");
+
+	// Differenz
     double diff = difftime(two_secs, now);
 	printf("%f\n\n", diff);
 }
