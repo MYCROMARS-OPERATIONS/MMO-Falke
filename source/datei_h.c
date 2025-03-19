4e41d973ef0b32ea5e4c984042e5c6a4d85a1794
Alexander Rudi <alex.r@mycromars-operations.com>
2025-03-19 23:56:23 +0100

    Copy String

--- source/datei_h.c
+++ source/datei_h.c
@@ -1,6 +1,7 @@
 // Datei h
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 void datei_h(void)
 {
@@ -22,11 +23,21 @@ void datei_h(void)
     printf("Now You are here:\n");
     printf("%s\n", getcwd(s, 100));
 
-
-    // Output to HTML
+    // String
     char siteFile[] = "index.html";
     char doctype[] = "<!DOCTYPE html>";
+    
+    // Copy a String *********
+    char scr[] = "copy this";
+    char dest[100];
+    strcpy(dest, scr);
 
+    printf("scr: %s\n", scr);
+    printf("dest: %s\n", dest);
+
+
+
+// File create/open and write **************
     FILE *FileX;
     FileX = fopen(siteFile, "w");
 
@@ -40,6 +51,8 @@ void datei_h(void)
         //puts("\nFile open");
         
         fprintf(FileX, "%s\n", doctype);
+        fprintf(FileX, "%s\n", scr);
+        fprintf(FileX, "%s\n", dest);
         puts("\nFile created successfully!");
         fclose(FileX);
         //puts("File close.\n");
