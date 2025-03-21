aefc4098992cb8b55808e029baea279432a25991
Alexander Rudi <alex.r@mycromars-operations.com>
2025-03-21 19:26:15 +0100

    Read String from file

--- source/datei_c.c
+++ source/datei_c.c
@@ -1,13 +1,18 @@
 // Datei c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Datei lesen Nummer (r)
 void datei_c(void)
 {
     int a = 0;
     int iNoRecords = 0;
+    char x[100];
 
     FILE *ReadFile;
     ReadFile = fopen("example.dat", "r");
-
+    /*
     if(ReadFile == NULL) 
     {
         puts("Fehler beim Öffnen von example.dat");
@@ -25,5 +30,57 @@ void datei_c(void)
         fclose(ReadFile);
         puts("Datei geschlossen.\n");
     }
+    */
+
+    // Read Character
+    /*
+    if (ReadFile != NULL)
+    {
+         printf("Read file c\n");
+         int c;
+         while ( (c = fgetc(ReadFile)) != EOF)
+             putchar(c);
+
+         fclose(ReadFile); 
+         printf("Ausgelesener Wert: %d\n", c);
+
+    } else printf("Error opening file.\n");
+    */
+
+
+    // Read String, Outout not correct
+    /*
+    if (ReadFile != NULL){
+         printf("Read file\n");
+         while ( (x[iNoRecords] = fgetc(ReadFile)) != EOF)
+         {
+             putchar(x[iNoRecords]);
+             iNoRecords++;
+         }
+         fclose(ReadFile); 
+         printf("\nAusgelesener Wert: %s\n", x);
+         printf("\n");
+
+    } else printf("Error opening file.\n");
+    */
+
+    // Read String
+    if(ReadFile == NULL) 
+     {
+         puts("Fehler beim Öffnen von example.dat");
+         exit (EXIT_FAILURE);
+     }
+     else
+     {
+         puts("Datei erfolgreich geoeffnet zum lesen.");
+         while (fscanf(ReadFile, "%s", &x[0]) !=EOF)
+         {
+              iNoRecords++;
+         }
+         printf("Ausgelesener Wert: %s\n", x);
+         printf("Records: %d\n", iNoRecords);
+         fclose(ReadFile);
+         puts("Datei geschlossen.\n\n");
+     }
     
 }
